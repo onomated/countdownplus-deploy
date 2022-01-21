f2a62d94d6d00e4295fc33692d96c584c2614872
Onome <onome@appsbeyond.com>
2022-01-21 14:10:28 -0600

    + Moved release artifacts to `release` directory + Updated docker files to work with new release directory structures

--- update-sources.sh
+++ update-sources.sh
@@ -2,13 +2,29 @@
 
 set -e
 
-echo 'Updating services source from countdownplus elixir release'
-if [ ! -d ../countdownplus-elixir/artifacts/releases/prod/ ]; then
-  echo 'Countdownplus elixir release does not exist'
+ELIXIR_DIR=countdownplus-elixir
+NEXTJS_DIR=countdownplus-nextjs
+
+echo "Updating services release artifacts from countdownplus elixir release"
+if [ ! -d ../$ELIXIR_DIR/artifacts/releases/prod/ ]; then
+  echo ''
+  echo "‼️ Countdownplus elixir release not found ‼️"
+  echo "‼️ Execute 'mix cplus.gen.release' from $ELIXIR_DIR root directory ‼️"
   exit -1
 fi
-cp -rf ../countdownplus-elixir/artifacts/releases/prod/ services/
+rm -rf services/release
+mkdir -p services/release
+cp -Rf ../$ELIXIR_DIR/artifacts/releases/prod/ services/release
 
-echo 'TODO: Updating frontend source from countdownplus nextjs build'
+echo 'Updating frontend release artifacts from countdownplus nextjs build'
+if [ ! -d ../$NEXTJS_DIR/artifacts/releases/prod/ ]; then
+  echo ''
+  echo "‼️ Countdownplus nextjs build directory does not exist ‼️"
+  echo "‼️ Execute './scripts/release.js' from $NEXTJS_DIR root directory ‼️"
+  exit -1
+fi
+rm -rf frontend/release
+mkdir -p frontend/release
+cp -Rf ../$NEXTJS_DIR/artifacts/releases/prod/ frontend/release
 
 echo 'Done'
